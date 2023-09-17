53571f6581e89b68a4d4aca571571f7d98be07ce
Colby <colby.wang765@gmail.com>
2023-09-17 19:55:31 -0400

    Login page almost done

---
+++ frontend/js/index.js
@@ -0,0 +1,51 @@
+function sendRequest(url, data, responseHandler) {
+    let request = new XMLHttpRequest();
+    request.open("POST", url);
+    request.setRequestHeader("Content-Type", "application/json");
+    request.send(data);
+
+    // When we receive a response from the server
+    // Handle the response with the function we pass in
+    request.onload = () => {
+        if (request.status === 200) {
+            responseHandler(request.responseText);
+        }
+    }
+}
+
+function handleLogin() {
+    // Grab username and password
+    const username = document.getElementById("usernameInput").value;
+    const password = document.getElementById("passwordInput").value;
+
+    // Grab the validation messages
+    const userValidation = document.getElementById("usernameValidation");
+    const passValidation = document.getElementById("passwordValidation");
+
+    // Reset the validation messages
+    userValidation.textContent = ""
+    passValidation.textContent = ""
+
+    // If either field is empty display a warning
+    if (username === "") {
+        userValidation.textContent = "Please enter a username"
+    }
+    if (password === "") {
+        passValidation.textContent = "Please enter a password"
+    }
+
+    if (username === "" && password === "") {
+        return;
+    }
+
+    const data = {
+        "username": username,
+        "password": password
+    }
+
+    function handleResponse (responseText) {
+        console.log(responseText)
+    }
+
+    sendRequest("/backend/Login.php", data, handleResponse);
+}
