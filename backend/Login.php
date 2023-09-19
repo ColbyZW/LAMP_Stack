b1b50bf773f4f304039d43aa40f8c4472dd00d97
Colby Wang <colby.wang765@gmail.com>
2023-09-19 18:30:23 -0400

    Test Login.php

--- backend/Login.php
+++ backend/Login.php
@@ -1,3 +1,19 @@
 <?php
-    echo "Login";
+    $inData = json_decode(file_get_contents('php://input'), true);
+    
+    $sqlConn = new mysqli("localhost", "root", "SPL-16P@ss", "contacts")
+
+    if ($sqlConn->connect_error) {
+        sendJson('{"success": false}')
+    }
+    $stmt = $sqlConn->prepare("SELECT * FROM contact_info")
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    sendJson($result)
+
+    function sendJson ($obj) {
+        header('Content-Type: application/json');
+        echo $obj;
+    }
 ?>
