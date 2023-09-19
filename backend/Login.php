2f5563c0937b4bb18ebdb265382ecacd9e216c08
Colby Wang <colby.wang765@gmail.com>
2023-09-19 19:56:42 -0400

    More SQL tests

--- backend/Login.php
+++ backend/Login.php
@@ -1,12 +1,12 @@
 <?php
     $inData = json_decode(file_get_contents('php://input'), true);
     
-    $sqlConn = new mysqli("localhost", "root", "SPL-16P@ss", "contacts");
+    $sqlConn = new mysqli("localhost", "root", "SPL-16P@ss", "COP4331");
 
     if ($sqlConn->connect_error) {
         sendJson('{"success": false}');
     }
-    $stmt = $sqlConn->prepare("SELECT * FROM contact_info");
+    $stmt = $sqlConn->prepare("SELECT * FROM users");
     $stmt->execute();
     $result = $stmt->get_result();
 
--- backend/Register.php
+++ backend/Register.php
@@ -1,3 +1,3 @@
 <?php
-    echo "Register";
+    print "Register";
 ?>
