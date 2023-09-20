23ed6669ed8238e9c4fcf281fd92303de0bccf9c
stephen <stephen.an@example.com>
2023-09-20 16:43:09 -0400

    changed checkpoint

--- backend/authenticate.php
+++ backend/authenticate.php
@@ -9,7 +9,7 @@
     //connect
     $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
     
-    echo 'authenticate.php checkpoint';
+    echo 'MySQL connected!';
     
     if ( mysqli_connect_errno() ) {
         // If there is an error with the connection, stop the script and display the error.
