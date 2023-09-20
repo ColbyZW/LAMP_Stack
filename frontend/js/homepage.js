decc70630dc9269b6788ec3d1cd2fcddb38eb579
Colby Wang <colby.wang765@gmail.com>
2023-09-20 13:56:46 -0400

    toast fixes

--- frontend/js/homepage.js
+++ frontend/js/homepage.js
@@ -1,14 +1,14 @@
 function onPageLoad() {
     const tableBody = document.getElementById("contactTable");
     const tableTitle = document.getElementById("tableBanner");
-    const toastText = document.getElementById("toastBodyText");
+    const toastText = document.getElementById("toastBodyText").value;
     const cookie = getCookie("username");
 
     if (cookie === "") {
         window.location.href = "index.html"
     } else {
         tableTitle.innerText = `Viewing ${cookie}\'s Contacts`;
-        toastText.innerText = `Successfully logged in as ${cookie}!`
+        toastText.textContent = `Successfully logged in as ${cookie}!`
     }
 
     function handleResponse (responseText) {
