19e59d6fc681cafb2bd2175ab3df1287a560f9ec
stephen <stephen.an@example.com>
2023-09-20 17:07:13 -0400

    test

--- backend/authenticate.php
+++ backend/authenticate.php
@@ -9,14 +9,16 @@
     //connect
     $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
     
-    echo 'MySQL connected!\n';
+    echo 'MySQL connected!';
+    echo "</br>"; 
     $temp = $_POST['username'];
     echo 'user name = ';
     echo $temp;
+    echo "</br>"; 
     $temp = $_POST['password'];
     echo 'password = ';
     echo $temp;
-    echo '\n';
+    echo "</br>"; 
     
     if ( mysqli_connect_errno() ) {
         // If there is an error with the connection, stop the script and display the error.
