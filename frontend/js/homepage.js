426e502396ea4ce3f31d7cfa2349227b7f3847f2
Colby Wang <colby.wang765@gmail.com>
2023-09-20 01:48:12 -0400

    Using cookies

--- frontend/js/homepage.js
+++ frontend/js/homepage.js
@@ -1,6 +1,13 @@
 function onPageLoad() {
     const tableBody = document.getElementById("contactTable");
+    const tableTitle = document.getElementById("tableTitle").value;
     const cookie = getCookie("username");
+
+    if (cookie === "") {
+        window.location.href = "index.html"
+    } else {
+        tableBody.textContent = `Viewing ${cookie}\'s Contacts`;
+    }
 
     function handleResponse (responseText) {
         const response = JSON.parse(responseText);
--- frontend/js/utility.js
+++ frontend/js/utility.js
@@ -28,11 +28,9 @@ function getRequest(url, param, responseHandler) {
 function getCookie(cookie) {
     const cookies = document.cookie;
     const cookieArray = cookies.split(";");
-    console.log(cookies);
-    console.log(cookieArray);
     for (let i = 0; i < cookieArray.length; i++) {
-        if (cookieArray[i] === cookie) {
-            const currentCookie = cookieArray[i].split("=");
+        const currentCookie = cookieArray[i].split("=");
+        if (currentCookie[0] === cookie) {
             return currentCookie[1];
         }
     }
