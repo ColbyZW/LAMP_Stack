aedf3c92492ab82110315ff3c3ff9857d94f68f9
Colby Wang <colby.wang765@gmail.com>
2023-09-20 14:05:50 -0400

    Removed the toast

--- frontend/js/homepage.js
+++ frontend/js/homepage.js
@@ -1,17 +1,12 @@
 function onPageLoad() {
     const tableBody = document.getElementById("contactTable");
     const tableTitle = document.getElementById("tableBanner");
-    const toastText = document.getElementById("toastBodyText");
     const cookie = getCookie("username");
-
-    const toastElList = document.querySelectorAll('.toast')
-    const toastList = [...toastElList].map(toastEl => new bootstrap.Toast(toastEl))
 
     if (cookie === "") {
         window.location.href = "index.html"
     } else {
         tableTitle.innerText = `Viewing ${cookie}\'s Contacts`;
-        toastText.innerText = `Successfully logged in as ${cookie}!`
     }
 
     function handleResponse (responseText) {
