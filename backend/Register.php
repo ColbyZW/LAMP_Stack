a23e4b58432016080c64c4476b273379f3e2967e
Colby Wang <colby.wang765@gmail.com>
2023-09-19 21:40:44 -0400

    Register php

--- backend/Register.php
+++ backend/Register.php
@@ -4,21 +4,36 @@
     $sqlConn = new mysqli("localhost", "root", "SPL-16P@ss", "COP4331");
 
     if ($sqlConn->connect_error) {
-        sendJson('{"success": false}');
+        sendJson('{"message": "Error connecting to database",
+                   "code": 401}');
     }
-    $stmt = $sqlConn->prepare("SELECT * FROM users");
+    $stmt = $sqlConn->prepare("SELECT * FROM users WHERE username = " . $inData["username"]);
     $stmt->execute();
     $result = $stmt->get_result();
 
-    $searchResults = "";
+    $matches = 0;
     while ( $row = $result->fetch_assoc() ) {
-        $searchResults .= $row["username"];
+        $matches++;
     }
 
-    sendJson($searchResults);
+    if ( $matches != 0 ) {
+        sendJson('{"message": "User with this name already exists",
+                   "code": 400}');
+    } else {
+        $stmt->close();
+        
+        $stmt = $sqlConn->prepare("INSERT INTO users(username,password) VALUES(?,?)");
+        $stmt->bind_param("ss", $inData["username"], $inData["password"]);
+        $stmt->execute();
+        $sqlConn->close();
 
-    $stmt->close();
-    $sqlConn->close();
+        sendJson('{"message": "Successfully created user",
+                   "code": "200",
+                   "username": ' . $inData["username"] . '}');
+    }
+
+
+
 
     function sendJson ($obj) {
         header('Content-Type: application/json');
