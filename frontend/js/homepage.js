8cd2f7e1952dc59e6f7db9cbf1d6c135b6ca0e3a
Colby Wang <colby.wang765@gmail.com>
2023-09-20 13:45:53 -0400

    Toast toggling

--- frontend/js/homepage.js
+++ frontend/js/homepage.js
@@ -2,11 +2,13 @@ function onPageLoad() {
     const tableBody = document.getElementById("contactTable");
     const tableTitle = document.getElementById("tableBanner");
     const toastText = document.getElementById("toastText");
+    const liveToast = document.getElementById("liveToast");
     const cookie = getCookie("username");
 
     if (cookie === "") {
         window.location.href = "index.html"
     } else {
+        bootstrap.Toast.getOrCreateInstance(liveToast).show();
         tableTitle.innerText = `Viewing ${cookie}\'s Contacts`;
         toastText.innerText = `Successfully logged in as ${cookie}!`
     }
