a93f0c4e1839c8805efc8e4fd3b03d3545021239
Colby Wang <colby.wang765@gmail.com>
2023-09-20 01:59:49 -0400

    Got the cookies working

--- frontend/js/homepage.js
+++ frontend/js/homepage.js
@@ -1,17 +1,17 @@
 function onPageLoad() {
     const tableBody = document.getElementById("contactTable");
+    const tableTitle = document.getElementById("tableBanner").value;
     const cookie = getCookie("username");
 
     if (cookie === "") {
         window.location.href = "index.html"
     } else {
-        const tableTitle = document.getElementById("tableBanner").value;
-        tableBody.textContent = `Viewing ${cookie}\'s Contacts`;
+        tableTitle.textContent = `Viewing ${cookie}\'s Contacts`;
     }
 
     function handleResponse (responseText) {
         const response = JSON.parse(responseText);
     }
 
-    getRequest("/backend/GetAll.php", "username=", handleResponse);
+    getRequest("/backend/GetAll.php", `username=${cookie}`, handleResponse);
 }
