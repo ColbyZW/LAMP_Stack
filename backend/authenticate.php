de7efff93a68c468ebf627528f4c3199c125ee16
stephen <stephen.an@example.com>
2023-09-20 17:15:49 -0400

    test

--- backend/authenticate.php
+++ backend/authenticate.php
@@ -26,7 +26,7 @@
     }
 
     // Check if the data from the login form was submitted, isset() will check if the data exists.
-    if ( !isset($_POST['username'], $_POST['password']) ) {
+    if (!isset($_POST['username'], $_POST['password'])) {
 	// Could not get the data that should have been sent.
 	exit('Please fill both the username and password fields!');
     
