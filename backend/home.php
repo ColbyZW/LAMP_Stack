4c2a41feba17bd3b20b8f8387c33e4834742e5a2
stephen <stephen.an@example.com>
2023-09-20 18:22:02 -0400

    Working login

--- backend/home.php
+++ backend/home.php
@@ -17,13 +17,12 @@ if (!isset($_SESSION['loggedin'])) {
 	<body class="loggedin">
 		<nav class="navtop">
 			<div>
-				<h1>Contact Manager</h1>
+				<h1>Contact Manager Home Page</h1>
 				<a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
 				<a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
 			</div>
 		</nav>
 		<div class="content">
-			<h2>Home Page</h2>
 			<p>Welcome back, <?=$_SESSION['name']?>!</p>
 		</div>
 	</body>
