9a03f92c0332f79b4715827e2f6b19045b5e651e
Colby Wang <colby.wang765@gmail.com>
2023-09-26 16:21:26 -0400

    Added form validation

--- frontend/js/homepage.js
+++ frontend/js/homepage.js
@@ -25,6 +25,41 @@ function onPageLoad() {
     getRequest("/backend/GetAll.php", `username=${cookie}`, handleResponse);
 }
 
+function addContact() {
+    const contactName = document.getElementById("contactName").value;
+    const contactNumber = document.getElementById("contactNumber").value;
+    const contactEmail = document.getElementById("contactEmail").value;
+
+    const nameError = document.getElementById("nameValidation");
+    const numberError = document.getElementById("phoneNumberValidation");
+    const emailError = document.getElementById("emailValidation");
+
+    nameError.textContent = "";
+    numberError.textContent = "";
+    emailError.textContent = "";
+
+    if (contactName === "") {
+        nameError.textContent = "Please enter a name";
+    }
+    if (contactNumber === "") {
+        numberError.textContent = "Please enter a phone number";
+    }
+    if (parseInt(contactNumber) === NaN) {
+        numberError.textContent = "Please enter a phone number with no dashes";
+    }
+    if (contactNumber.length != 10) {
+        numberError.textContent = "Please enter a valid phone number";
+    }
+    if (contactEmail === "") {
+        emailError.textContent = "Please enter an email";
+    }
+
+    if (emailError.textContent != "" || numberError.textContent != "" || nameError.textContent != "") {
+        return;
+    }
+
+}
+
 function handleLogout() {
     document.cookie = "username=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
     window.location.href = "index.html"
