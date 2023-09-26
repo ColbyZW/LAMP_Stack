2c6d6c308ab0d9f481081860394d42b2d7b97cf1
Eduardo Vila <eduardovila57@gmail.com>
2023-09-26 19:05:14 -0400

    php for the add contacts page

--- backend/AddContact.php
+++ backend/AddContact.php
@@ -1,14 +1,15 @@
 <?php
     $inData = json_decode(file_get_contents('php://input'), true);
     
-    $sqlConn = new mysqli("localhost", "root", "YourPassword", "YourDatabase");
+    $sqlConn = new mysqli("localhost", "root", "SPL-16P@SS", "COP4331");
     
     if ($sqlConn->connect_error) {
         sendJson('{"message": "Error connecting to database", "code": 401}');
     }
-    
-    $stmt = $sqlConn->prepare("INSERT INTO contact_info (username, contactName, contactNumber) VALUES (?, ?, ?)");
-    $stmt->bind_param("sss", $inData["username"], $inData["contactName"], $inData["contactNumber"]);
+    $uuid = uniqid();
+
+    $stmt = $sqlConn->prepare("INSERT INTO contact_info (username, contactName, contactNumber, uuid) VALUES (?, ?, ?, ?)");
+    $stmt->bind_param("ssss", $inData["username"], $inData["contactName"], $inData["contactNumber"], $uuid);
     $stmt->execute();
     
     sendJson('{"message": "Successfully added contact", "code": 200}');
