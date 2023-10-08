048cbdd29eb2de14eb6858ebf1fa51dec1bfd9a2
stephen <stephen.an@example.com>
2023-10-07 20:14:25 -0400

    test

--- frontend/js/homepage.js
+++ frontend/js/homepage.js
@@ -238,6 +238,6 @@ function addContact() {
 // Handles logging out
 function handleLogout() {
     document.cookie = "username=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
-    transitionToPage('./index.html')
+    transitionToPage('index.html')
     //window.location.href = "index.html"
 }
